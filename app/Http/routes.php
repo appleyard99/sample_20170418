9a7b4e31f220967bf1f34dc3f899680eb601f57c
appleyard99 <apple_mgg@sina.com>
2017-04-28 08:49:50 +0000

    finish static pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,8 +9,11 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/','StaticPagesController@home'); //url '/'以get方式发出的请求将有StaticPagesController控制器中的home方法处理;
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
